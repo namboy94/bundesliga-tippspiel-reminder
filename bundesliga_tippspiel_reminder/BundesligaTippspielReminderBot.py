b33328f9e094493ecc61eecee4b59ed9a8708fb8
Hermann Krumrey <hermann@krumreyh.com>
2019-09-13 18:35:54 +0200

    Integrated new features from kudubot

--- bundesliga_tippspiel_reminder/BundesligaTippspielReminderBot.py
+++ bundesliga_tippspiel_reminder/BundesligaTippspielReminderBot.py
@@ -17,13 +17,13 @@ You should have received a copy of the GNU General Public License
 along with btr.  If not, see <http://www.gnu.org/licenses/>.
 LICENSE"""
 
-import time
 from typing import List, Dict, Any, Optional
 from sqlalchemy.orm import Session
 from bokkichat.entities.message.TextMessage import TextMessage
 from kudubot.Bot import Bot
 from kudubot.db.Address import Address as Address
 from kudubot.parsing.CommandParser import CommandParser
+from bundesliga_tippspiel_reminder import version
 from bundesliga_tippspiel_reminder.db.ApiKey import ApiKey
 from bundesliga_tippspiel_reminder.db.Reminder import Reminder
 from bundesliga_tippspiel_reminder.BundesligaTippspielReminderParser import \
@@ -36,53 +36,6 @@ class BundesligaTippspielReminderBot(Bot):
     The bundesliga tippspiel reminder bot
     """
 
-    def on_command(
-            self,
-            message: TextMessage,
-            parser: CommandParser,
-            command: str,
-            args: Dict[str, Any],
-            sender: Address,
-            db_session: Session
-    ):
-        """
-        Defines the behaviour of the bot when receiving a message
-        :param message: The received message
-        :param parser: The parser that matched the message
-        :param command: The command that matched the message
-        :param args: The arguments of the command
-        :param sender: The sender of the message
-        :param db_session: The database session to use
-        :return: None
-        """
-        if command == "login":
-            self._handle_login(sender, args, db_session)
-            return
-        elif command == "is_authorized":
-            self._handle_is_authorized(sender, db_session)
-            return
-
-        # Everything beyond this requires authorization
-        api_key = self._get_api_key(sender, db_session)
-        if api_key is None:
-            self.connection.send(TextMessage(
-                self.connection.address,
-                sender,
-                "Not authorized, use /login <username> <password> first",
-                "Not authorized"
-            ))
-            return
-
-        reminder = self._get_reminder(sender, db_session)
-        if command == "leaderboard":
-            self._handle_leaderboard(sender, api_key)
-        elif command == "reminder_state":
-            self._handle_reminder_state(sender, reminder)
-        elif command == "activate_reminder":
-            self._handle_activate_reminder(sender, args, reminder, db_session)
-        elif command == "deactivate_reminder":
-            self._handle_deactivate_reminder(sender, reminder, db_session)
-
     @classmethod
     def name(cls) -> str:
         """
@@ -91,19 +44,50 @@ class BundesligaTippspielReminderBot(Bot):
         return "bundesliga-tippspiel-reminder"
 
     @classmethod
+    def version(cls) -> str:
+        """
+        :return: The current version of the bot
+        """
+        return version
+
+    @property
+    def bg_pause(self) -> int:
+        """
+        The pause between background iterations
+        :return: The pause in seconds
+        """
+        return 600
+
+    @classmethod
     def parsers(cls) -> List[CommandParser]:
         """
         :return: The parsers for the bot
         """
         return [BundesligaTippspielReminderParser()]
 
+    def is_authorized(
+            self,
+            address: Address,
+            _: Dict[str, Any],
+            db_session: Session
+    ) -> bool:
+        """
+        Checks if a user is authorized
+        :param address: The user to check
+        :param _: possible command arguments
+        :param db_session: The database session to use
+        :return: True if authorized, False otherwise
+        """
+        return self._get_api_key(address, db_session) is None
+
     @classmethod
-    def extra_config_args(cls) -> List[str]:
+    def unauthorized_message(cls) -> str:
         """
-        :return: A list of additional settings parameters required for
-                 this bot. Will be stored in a separate extras.json file
+        :return: A custom message sent to a user if they tried to access
+                 a feature that requires authorization without being
+                 authorized
         """
-        return []
+        return "Not authorized, use /login <username> <password> first"
 
     @staticmethod
     def _get_api_key(address: Address, db_session: Session) \
@@ -130,7 +114,7 @@ class BundesligaTippspielReminderBot(Bot):
         return db_session.query(Reminder).filter_by(kudubot_user=address)\
             .first()
 
-    def _handle_login(
+    def _on_login(
             self,
             sender: Address,
             args: Dict[str, Any],
@@ -162,10 +146,16 @@ class BundesligaTippspielReminderBot(Bot):
         reply = TextMessage(self.connection.address, sender, reply, "Login")
         self.connection.send(reply)
 
-    def _handle_is_authorized(self, sender: Address, db_session: Session):
+    def _on_is_authorized(
+            self,
+            sender: Address,
+            _: Dict[str, Any],
+            db_session: Session
+    ):
         """
         Handles an is_authorized command
         :param sender: The sender of the message
+        :param _: The command arguments
         :param db_session: The database session to use
         :return: None
         """
@@ -175,13 +165,21 @@ class BundesligaTippspielReminderBot(Bot):
             self.connection.address, sender, reply, "Authorized"
         ))
 
-    def _handle_leaderboard(self, sender: Address, api_key: str):
+    @Bot.auth_required
+    def _on_leaderboard(
+            self,
+            address: Address,
+            _: Dict[str, Any],
+            db_session: Session
+    ):
         """
         Handles a leaderboard command
-        :param sender: The sender of the message
-        :param api_key: The API key to use
+        :param address: The sender of the command
+        :param _: Command arguments
+        :param db_session: The database session to use
         :return: None
         """
+        api_key = self._get_api_key(address, db_session)
         response = api_request("leaderboard", "get", {}, api_key)
 
         if response["status"] == "ok":
@@ -193,52 +191,60 @@ class BundesligaTippspielReminderBot(Bot):
                     user["username"],
                     points
                 ))
+            self.send_txt(address, "\n".join(formatted), "Leaderboard")
 
-            reply = "\n".join(formatted)
-            self.connection.send(TextMessage(
-                self.connection.address, sender, reply, "Leaderboard"
-            ))
-
-    def _handle_reminder_state(
+    @Bot.auth_required
+    def _on_reminder_state(
             self,
-            sender: Address,
-            reminder: Optional[Reminder]
+            address: Address,
+            _: Dict[str, Any],
+            db_session: Session
     ):
         """
         Handles a reminder_state command
-        :param sender: The sender of the command
-        :param reminder: The reminder for that user
+        :param address: The sender of the command
+        :param _: Command arguments
+        :param db_session: The database session to use
         :return: None
         """
+        reminder = self._get_reminder(address, db_session)
         if reminder is None:
             reply = "No reminder set"
         else:
             reply = "Reminder set to go off {} hours before a match.".format(
                 int(reminder.reminder_time / 3600)
             )
-        self.connection.send(TextMessage(
-            self.connection.address, sender, reply, "Reminder State"
-        ))
+        self.send_txt(address, reply, "Reminder State")
 
-    def _handle_activate_reminder(
+    @Bot.auth_required
+    def _on_activate_reminder(
             self,
-            sender: Address,
+            address: Address,
             args: Dict[str, Any],
-            reminder: Optional[Reminder],
             db_session: Session
     ):
+        """
+        Handles activating reminders
+        :param address: The sender of the message
+        :param args: Command arguments
+        :param db_session: The database session to use
+        :return: None
+        """
+        reminder = self._get_reminder(address, db_session)
+
         hours = args["hours"]
         seconds = hours * 3600
         if hours < 1 or hours > 120:
-            self.connection.send(TextMessage(
-                self.connection.address, sender,
-                "Reminders can only be 1-120 hours", "Invalid Reminder Time"
-            ))
+            self.send_txt(
+                address,
+                "Reminders can only be 1-120 hours",
+                "Invalid Reminder Time"
+            )
             return
 
         if reminder is None:
             reminder = Reminder(
-                kudubot_user=sender,
+                kudubot_user=address,
                 reminder_time=seconds
             )
             db_session.add(reminder)
@@ -247,74 +253,68 @@ class BundesligaTippspielReminderBot(Bot):
 
         db_session.commit()
 
-        self.connection.send(TextMessage(
-            self.connection.address, sender,
-            "Reminder set to {} hours".format(hours), "Reminder Time set"
-        ))
+        self.send_txt(
+            address,
+            "Reminder set to {} hours".format(hours),
+            "Reminder Time set"
+        )
 
-    def _handle_deactivate_reminder(
+    @Bot.auth_required
+    def _on_deactivate_reminder(
             self,
-            sender: Address,
-            reminder: Optional[Reminder],
+            address: Address,
+            _: Dict[str, Any],
             db_session: Session
     ):
         """
         Deactivates a reminder
-        :param sender: The sender of the message
-        :param reminder: The previously existing reminder
+        :param address: The sender of the message
+        :param _: Command arguments
         :param db_session: The database session to use
         :return: None
         """
+        reminder = self._get_reminder(address, db_session)
         if reminder is not None:
             db_session.delete(reminder)
             db_session.commit()
-        self.connection.send(TextMessage(
-            self.connection.address, sender,
-            "Reminder Deactivated", "Reminder Deactivated"
-        ))
+        self.send_txt(address, "Reminder Deactivated", "Reminder Deactivated")
 
-    def run_in_bg(self):
+    def bg_iteration(self, _: int, db_session: Session):
         """
-        Background thread that periodically checks if any reminders are due
+        Implements behaviours of the Background thread that periodically
+        checks if any reminders are due
         :return: None
         """
-        self.logger.info("Starting background thread")
-        while True:
-            self.logger.info("Checking for due reminders")
+        self.logger.info("Checking for due reminders")
 
-            db_session = self.sessionmaker()
-            matches = None  # Refresh matches each iteration
+        for reminder in db_session.query(Reminder).all():
+            api_key = self._get_api_key(reminder.kudubot_user, db_session)
 
-            for reminder in db_session.query(Reminder).all():
-                api_key = self._get_api_key(reminder.kudubot_user, db_session)
+            if not api_is_authorized(api_key):
+                continue
 
-                if not api_is_authorized(api_key):
-                    continue
+            resp = api_request("match", "get", {}, api_key)
+            matches = resp["data"]["matches"]
 
-                if matches is None:
-                    resp = api_request("match", "get", {}, api_key)
-                    matches = resp["data"]["matches"]
-                bets = api_request("bet", "get", {}, api_key)["data"]["bets"]
-                due = reminder.get_due_matches(matches, bets)
+            bets = api_request("bet", "get", {}, api_key)["data"]["bets"]
+            due = reminder.get_due_matches(matches, bets)
 
-                if len(due) > 0:
-                    body = "Reminders for hk-tippspiel.com:\n\n"
-                    for match in due:
-                        body += "{} vs. {}\n".format(
-                            match["home_team"]["name"],
-                            match["away_team"]["name"]
-                        )
-                    msg = TextMessage(
-                        self.connection.address,
-                        reminder.kudubot_user,
-                        body,
-                        "Reminders"
+            if len(due) > 0:
+                body = "Reminders for hk-tippspiel.com:\n\n"
+                for match in due:
+                    body += "{} vs. {}\n".format(
+                        match["home_team"]["name"],
+                        match["away_team"]["name"]
                     )
-                    self.connection.send(msg)
-                    last_match = max(due, key=lambda x: x["kickoff"])
-                    reminder.last_reminder = last_match["kickoff"]
-                    db_session.commit()
+                msg = TextMessage(
+                    self.connection.address,
+                    reminder.kudubot_user,
+                    body,
+                    "Reminders"
+                )
+                self.connection.send(msg)
+                last_match = max(due, key=lambda x: x["kickoff"])
+                reminder.last_reminder = last_match["kickoff"]
+                db_session.commit()
 
-            self.logger.info("Finished checking for due reminders")
-            self.sessionmaker.remove()
-            time.sleep(3600)
+        self.logger.info("Finished checking for due reminders")
